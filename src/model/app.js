5df0c47381b94ce30489d4098f58a326a4a39fc7
Samuel Douay <douaysamuel@gmail.com>
2023-10-21 00:29:04 +0200

    finish static

---
+++ src/model/app.js
@@ -0,0 +1,56 @@
+import THREE from 'treejs';
+
+// ThreeJS scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(50,window.innerWidth/window.innerHeight,0.1,1000);
+camera.position.z = 30;
+
+// Web GL renderer
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Light
+const light = new THREE.AmbientLight(0x404040, 1);
+scene.add(light);
+// Directional lights
+const directionalLight = new THREE.DirectionalLight(0xFF245E, 2);
+directionalLight.position.set(0, 1, 9);
+scene.add(directionalLight);
+const directionalLight2 = new THREE.DirectionalLight(0x2EFF96, .10);
+directionalLight2.position.set(-10, 0, -2);
+scene.add(directionalLight2);
+const directionalLight3 = new THREE.DirectionalLight(0xFFF92E, 2);
+directionalLight3.position.set(-4, 2, 1);
+scene.add(directionalLight3);
+
+// 
+
+// GLTF Loader
+const gltfLoader = new THREE.GLTFLoader();
+gltfLoader.load('./asset/model/cats.gltf', (gltf) => {
+    const root = gltf.scene;
+    root.position.set(0,0,0);
+    root.rotation.y = Math.PI * -0.3;
+    root1 = root.getObjectByName('cats');
+    scene.add(root);
+})
+
+//Orbit controls
+const controls = new THREE.OrbitControls(camera);
+controls.Zoom = true;
+controls.Pan = true;
+controls.Rotate = true;
+
+// Animate function
+const animate = function(){
+    requestAnimationFrame(animate);
+    if(root1){
+        root1.rotation.y += 0.05;
+    }
+    controls.update();
+    renderer.render(scene,camera);
+};
+animate();
--- src/pages/Presentation.jsx
+++ src/pages/Presentation.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import DataContext from "../context/dataContext";
 import IconComponant from "../components/IconComponant";
+import { ArrowDownToLine } from 'lucide-react';
 
 const Presentation = () => {
   const { presentation } = useContext(DataContext);
@@ -16,13 +17,17 @@ const Presentation = () => {
           </span> <br/>
           aka <span>xviinash</span>
         </div>
-        <h4 id="activite_presentation">{presentation.activite}</h4>
+        <p id="activite_presentation">{presentation.activite}</p>
       </div>
       <div>
         <section>
           <h2>Turn me :) (it's comming)</h2>
           <img src={presentation.photo} alt={presentation.prenom} />
         </section>
+        <div id="lien_pdf">
+          <a href="#"><ArrowDownToLine /> Portfolio [FR] (PDF)</a>
+          <a href="#"><ArrowDownToLine /> Curriculum vitæ (PDF)</a>
+        </div>
       </div>
       <div id="reseaux_container">
         {presentation.resaux.map((e, index) => (
