704b1be9b3c01e422964a314efd82656252b69f6
Samuel Douay <douaysamuel@gmail.com>
2023-11-26 11:56:44 +0100

    perf

--- src/components/PortefolioComponent.jsx
+++ src/components/PortefolioComponent.jsx
@@ -5,7 +5,7 @@ const PortefolioComponent = ({ project }) => {
 
   return (
     <a href={project.url} target="_blank">
-      <img src={project.covers.original_webp} alt={project.slug} />
+      <img src={project.covers['404_webp']} alt={project.slug} />
       <div className="project_footer">
         <div className="arrow_project"> <ArrowDownRight /> Open with Behance</div>
         <h4>{project.name}</h4>
